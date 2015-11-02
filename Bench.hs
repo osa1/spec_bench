97390f3990534fc6bca2f2c9a466faeec50657c4
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-11-02 09:31:34 -0500

    Improving the benchmark:
    Standard lists' sum function turned out to be extra slow, probably because of
    extra dispatch for `Num`(investigating). For fair comparison I implemented all
    variants manually.

--- Bench.hs
+++ Bench.hs
@@ -6,35 +6,111 @@ import Control.DeepSeq
 import Criterion
 import Criterion.Main
 
-data IntList = ILNil | ILCons {-# UNPACK #-} !Int IntList
+main :: IO ()
+main = defaultMain $ concatMap mkbench [1..7 :: Int]
+-- main = defaultMain $ concatMap mkbench [6]
+
+mkbench :: Int -> [Benchmark]
+mkbench size =
+    [ env (generateBaseList size) $ \lst ->
+        bgroup ("Haskell list (size: 10^" ++ show size ++ ")")
+          [ bench "length" (nf length lst)
+          , bench "sum" (nf sum lst)
+          ]
+
+    , env (generateGL size) $ \lst ->
+        bgroup ("Generic list (size: 10^" ++ show size ++ ")")
+          [ bench "length" (nf glLength lst)
+          , bench "sum" (nf glSum lst)
+          ]
+
+    , env (generateSGL size) $ \lst ->
+        bgroup ("Generic strict list (size: 10^" ++ show size ++ ")")
+          [ bench "length" (nf sglLength lst)
+          , bench "sum" (nf sglSum lst)
+          ]
+
+    , env (generateSpecList size) $ \lst ->
+        bgroup ("Unboxed Int list (size: 10^" ++ show size ++ ")")
+          [ bench "length" (nf specListLength lst)
+          , bench "sum" (nf specListSum lst)
+          ]
+    ]
+
+--------------------------------------------------------------------------------
+-- * Haskell's list
+
+generateBaseList :: Int -> IO [Int]
+generateBaseList size = return [0 .. (10 ^ size)]
+
+--------------------------------------------------------------------------------
+-- * Operations on lazy generic lists
+
+data GL a = GLNil | GLCons a (GL a)
+  deriving (Show)
+
+instance NFData a => NFData (GL a) where
+  rnf GLNil        = ()
+  rnf (GLCons h t) = rnf h `seq` rnf t
+
+generateGL :: Int -> IO (GL Int)
+generateGL size = return $ foldr GLCons GLNil [0 .. (10 ^ size)]
+
+glLength :: GL a -> Int
+glLength = glLengthAcc 0
+
+glLengthAcc :: Int -> GL a -> Int
+glLengthAcc !acc GLNil        = acc
+glLengthAcc !acc (GLCons _ l) = glLengthAcc (acc + 1) l
+
+glSum :: GL Int -> Int
+glSum = glSumAcc 0
+
+glSumAcc :: Int -> GL Int -> Int
+glSumAcc !acc GLNil        = acc
+glSumAcc !acc (GLCons h t) = glSumAcc (acc + h) t
+
+--------------------------------------------------------------------------------
+-- * Strict generic lists
+
+data SGL a = SGLNil | SGLCons !a !(SGL a)
+  deriving (Show)
+
+instance NFData a => NFData (SGL a) where
+  rnf SGLNil        = ()
+  rnf (SGLCons h t) = rnf h `seq` rnf t
+
+generateSGL :: Int -> IO (SGL Int)
+generateSGL size = return $ foldr SGLCons SGLNil [0 .. (10 ^ size)]
+
+sglLength :: SGL a -> Int
+sglLength = sglLengthAcc 0
+
+sglLengthAcc :: Int -> SGL a -> Int
+sglLengthAcc !acc SGLNil        = acc
+sglLengthAcc !acc (SGLCons _ l) = sglLengthAcc (acc + 1) l
+
+sglSum :: SGL Int -> Int
+sglSum = sglSumAcc 0
+
+sglSumAcc :: Int -> SGL Int -> Int
+sglSumAcc !acc SGLNil        = acc
+sglSumAcc !acc (SGLCons h t) = sglSumAcc (acc + h) t
+
+--------------------------------------------------------------------------------
+-- * Specialized strict lists
+
+data IntList
+  = ILNil
+  | ILCons {-# UNPACK #-} !Int {-# UNPACK #-} !IntList
   deriving (Show)
 
 instance NFData IntList where
   rnf ILNil        = ()
   rnf (ILCons _ l) = rnf l
 
-main :: IO ()
-main = defaultMain
-    [ env generateGenericList $ \lst ->
-        bgroup "Generic list"
-          [ bench "length" (nf length lst)
-          , bench "sum" (nf sum lst)
-          ]
-    , env generateSpecList $ \lst ->
-        bgroup "Specialized Int list"
-          [ bench "length" (nf specListLength lst)
-          , bench "sum" (nf specListSum lst)
-          ]
-    ]
-
-size :: Int
-size = 10 ^ (6 :: Int)
-
-generateGenericList :: IO [Int]
-generateGenericList = return [0 .. size]
-
-generateSpecList :: IO IntList
-generateSpecList = return $ mkSpecList 0 (size + 1)
+generateSpecList :: Int -> IO IntList
+generateSpecList size = return $ mkSpecList 0 ((10 ^ size) + 1)
   where
     mkSpecList i b
       | i == b    = ILNil
@@ -48,7 +124,7 @@ specListLengthAcc !acc ILNil = acc
 specListLengthAcc !acc (ILCons _ l) = specListLengthAcc (acc + 1) l
 
 specListSum :: IntList -> Int
-specListSum lst = specListSumAcc 0 lst
+specListSum = specListSumAcc 0
 
 specListSumAcc :: Int -> IntList -> Int
 specListSumAcc !acc ILNil = acc
